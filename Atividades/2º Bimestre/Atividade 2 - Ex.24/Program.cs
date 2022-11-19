00d9e7974e7be82ed9746f804dfad211c0573e8f
brendonfeitosa <brendonfeitosa@hotmail.com>
2022-11-19 10:30:47 -0300

    Començando a resolução

--- Atividades/2º Bimestre/Atividade 2 - Ex.24/Program.cs
+++ Atividades/2º Bimestre/Atividade 2 - Ex.24/Program.cs
@@ -4,9 +4,6 @@ b) Pesquisar um valor digitado pelo usuário. Exiba uma mensagem informando se e
 c) Remover um valor digitado pelo usuário. Exiba a mensagem se removido com sucesso ou não encontrado
 d) Exibir todos os valores da árvore em ordem, pré ordem ou pós ordem*/
 
-
-
-
 void Insere(ref tp_no raiz, int valor)
 {
    if (raiz == null)
@@ -32,20 +29,71 @@ tp_no Busca(tp_no raiz, int valor)
       return Busca(raiz.dir, valor);
 }
 
-tp_no RetornaMaior(ref tp_no r)
+tp_no RetornaMaior(ref tp_no raiz)
 {
-   if (r.dir == null)
+   if (raiz.dir == null)
    {
-      tp_no p = r;
-      r = r.esq;
+      tp_no p = raiz;
+      raiz = raiz.esq;
       return p;
    }
    else
-      return RetornaMaior(ref r.dir);
+      return RetornaMaior(ref raiz.dir);
 }
 
+tp_no Remove(ref tp_no raiz, int x)
+{
+   if (raiz == null)
+      return null;     
+   else if (x == raiz.valor)
+   {       
+      tp_no p = raiz;
+      if (raiz.esq == null)        // nao tem filho esquerdo
+         raiz = raiz.dir;
+      else if (raiz.dir == null)  // nao tem filho direito
+         raiz = raiz.esq;
+      else                          // tem ambos os filhos
+      {
+         p = RetornaMaior(ref raiz.esq);
+         raiz.valor = p.valor;
+      }
+      return p;
+   }
+   else if (x < raiz.valor)
+      return Remove(ref raiz.esq, x);
+   else
+      return Remove(ref raiz.dir, x);
+}
 
+void EmOrdem(tp_no r)
+{
+   if (r != null)
+   {
+      EmOrdem(r.esq);
+      Console.WriteLine(r.valor);
+      EmOrdem(r.dir);
+   }
+}
 
+void PreOrdem(tp_no r)
+{
+   if (r != null)
+   {
+      Console.WriteLine(r.valor);
+      PreOrdem(r.esq);
+      PreOrdem(r.dir);
+   }
+}
+
+void PosOrdem(tp_no r)
+{
+   if (r != null)
+   {
+      PosOrdem(r.esq);
+      PosOrdem(r.dir);
+      Console.WriteLine(r.valor);
+   }
+}
 int Menu()
 {
     int opcao = 0;
@@ -60,3 +108,15 @@ int Menu()
     Console.WriteLine("\n------------------------------------------------------------------\n");
     return opcao;
 }
+tp_no raiz = null;
+
+class tp_no
+{
+   public tp_no esq;
+   public int valor;
+   public tp_no dir;
+}
+
+
+
+
