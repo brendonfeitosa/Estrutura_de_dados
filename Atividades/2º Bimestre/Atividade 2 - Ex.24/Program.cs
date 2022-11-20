8171b231de8d31e3e3ba0ad380b154ba18e03b63
brendonfeitosa <brendonfeitosa@hotmail.com>
2022-11-20 09:53:00 -0300

    quase pronto

--- Atividades/2º Bimestre/Atividade 2 - Ex.24/Program.cs
+++ Atividades/2º Bimestre/Atividade 2 - Ex.24/Program.cs
@@ -15,7 +15,7 @@ while(true)
       Console.Write("\nDigite um valor para ser incluído na árvore: ");
       int valor = Convert.ToInt32(Console.ReadLine());
       Insere(ref raiz, valor);
-      Console.WriteLine("");
+      Console.WriteLine("\nValor inserido!\n");
    }
 
    else if(op == 2)
@@ -26,15 +26,80 @@ while(true)
       Busca(raiz, valor_pesq);
       if(valor_pesq == raiz.valor || valor_pesq == raiz.esq.valor || valor_pesq == raiz.dir.valor)
       {
-         Console.WriteLine("Valor encontrado!");
+         Console.WriteLine("\nValor encontrado!\n");
       }
       else
       {
-         Console.WriteLine("Valor não encontrado");
+         Console.WriteLine("\nValor não encontrado\n"); //quando não encontro o valor o programa quebra
       }
    }
+   else if(op == 3)
+   {
+      Console.WriteLine("************************ REMOVER ************************");
+      Console.Write("\nDigite o valor que deseja remover: ");
+      int valor_remov = int.Parse(Console.ReadLine());
+      Busca(raiz, valor_remov);
+      if(valor_remov == raiz.valor || valor_remov == raiz.esq.valor || valor_remov == raiz.dir.valor)
+      {
+         Console.WriteLine("\nValor encontrado!");
+         Remove(ref raiz, valor_remov);
+         Thread.Sleep(1500);
+         Console.WriteLine("\nO valor informado foi removido com sucesso!\n");
+      }
+      else
+      {
+         Console.WriteLine("\nValor não encontrado");
+      }
+   }
+   else if(op == 4)
+   {
+      Console.WriteLine("************************ EXIBIR ************************");
+      Console.WriteLine("\nComo deseja exibir os dados?\n");
+      Console.WriteLine("[1] Em ordem");
+      Console.WriteLine("[2] Pré ordem");
+      Console.WriteLine("[3] Pós ordem");
+      Console.WriteLine("[4] Digite 4 para sair");
+      Console.Write("\nDigite a opção desejada: ");
+      int op_exibicao = Convert.ToInt32(Console.ReadLine());
+      if(raiz != null)
+         if(op_exibicao == 1)
+         {
+            Console.WriteLine("\n************************ EXIBIR EM ORDEM ************************\n");
+            EmOrdem(raiz);
+            Console.WriteLine("");
+            Console.WriteLine("");
+         }
+         else if(op_exibicao == 2)
+         {
+            Console.WriteLine("\n********************** EXIBIR EM PRÉ ORDEM **********************\n");
+            PreOrdem(raiz);
+            Console.WriteLine("");
+            Console.WriteLine("");
+         }
+         else if(op_exibicao == 3)
+         {
+            Console.WriteLine("\n********************** EXIBIR EM PÓS ORDEM **********************\n");
+            PosOrdem(raiz);
+            Console.WriteLine("");
+            Console.WriteLine("");
+         }  
+         else if(op_exibicao == 4)
+         {
+            Console.WriteLine("\nSaindo do modo de exibição\n");
+         }       
+      else
+      {
+         Console.WriteLine("\nA raiz esta vazia, náo ha nada a exibir\n");
+      }   
+   }
+   else if(op == 5)
+   {
+      Console.WriteLine("Saindo...");
+      Thread.Sleep(500);
+      Console.WriteLine("Fim do programa!");
+      break;
+   }
 }
-
 
 void Insere(ref tp_no raiz, int valor)
 {
@@ -73,57 +138,57 @@ tp_no RetornaMaior(ref tp_no raiz)
       return RetornaMaior(ref raiz.dir);
 }
 
-tp_no Remove(ref tp_no raiz, int x)
+tp_no Remove(ref tp_no raiz, int valor_remov)
 {
    if (raiz == null)
       return null;     
-   else if (x == raiz.valor)
+   else if (valor_remov == raiz.valor)
    {       
-      tp_no p = raiz;
+      tp_no ponteiro = raiz;
       if (raiz.esq == null)        // nao tem filho esquerdo
          raiz = raiz.dir;
       else if (raiz.dir == null)  // nao tem filho direito
          raiz = raiz.esq;
       else                          // tem ambos os filhos
       {
-         p = RetornaMaior(ref raiz.esq);
-         raiz.valor = p.valor;
+         ponteiro = RetornaMaior(ref raiz.esq);
+         raiz.valor = ponteiro.valor;
       }
-      return p;
+      return ponteiro;
    }
-   else if (x < raiz.valor)
-      return Remove(ref raiz.esq, x);
+   else if (valor_remov < raiz.valor)
+      return Remove(ref raiz.esq, valor_remov);
    else
-      return Remove(ref raiz.dir, x);
+      return Remove(ref raiz.dir, valor_remov);
 }
 
-void EmOrdem(tp_no r)
+void EmOrdem(tp_no raiz)
 {
-   if (r != null)
+   if (raiz != null)
    {
-      EmOrdem(r.esq);
-      Console.WriteLine(r.valor);
-      EmOrdem(r.dir);
+      EmOrdem(raiz.esq);
+      Console.Write(raiz.valor+" ");
+      EmOrdem(raiz.dir);
    }
 }
 
-void PreOrdem(tp_no r)
+void PreOrdem(tp_no raiz)
 {
-   if (r != null)
+   if (raiz != null)
    {
-      Console.WriteLine(r.valor);
-      PreOrdem(r.esq);
-      PreOrdem(r.dir);
+      Console.Write(raiz.valor+" ");
+      PreOrdem(raiz.esq);
+      PreOrdem(raiz.dir);
    }
 }
 
-void PosOrdem(tp_no r)
+void PosOrdem(tp_no raiz)
 {
-   if (r != null)
+   if (raiz != null)
    {
-      PosOrdem(r.esq);
-      PosOrdem(r.dir);
-      Console.WriteLine(r.valor);
+      PosOrdem(raiz.esq);
+      PosOrdem(raiz.dir);
+      Console.WriteLine(raiz.valor+" ");
    }
 }
 int Menu()
