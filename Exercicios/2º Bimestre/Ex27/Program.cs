5d499ce7b99bdd24452ee6ad3c628849afed0696
brendonfeitosa <brendonfeitosa@hotmail.com>
2022-11-24 21:14:38 -0300

    Iniciando o exercício 27 Hash

---
+++ Exercicios/2º Bimestre/Ex27/Program.cs
@@ -0,0 +1,3 @@
+﻿/*27) Implemente um programa que conte a quantidade de colisões ocorridas durante o processo de inserção. Utilize o tratamento de colisão linear. O vetor deve ser de um tipo abstrato de dado composto por nota, nome e email. A nota deve ser um número inteiro e corresponderá a chave.
+O menu deve conter as seguintes opções: Inserir, Recuperar e Informar. Na opção recuperar, solicite a nota e caso encontre-a no vetor, exiba o nome e o email correspondente, caso contrário, exiba a mensagem de nota não encontrada. A opção informar, informa a quantidade de colisões ocorridas até o momento.*/
+
